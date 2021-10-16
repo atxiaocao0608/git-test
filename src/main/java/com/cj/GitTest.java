6aa6c00e003df9a2a73655aa1bc241bdbe1de274
cj <cj@qq.com>
2021-10-16 10:39:49 +0800

    first commit

---
+++ src/main/java/com/cj/GitTest.java
@@ -0,0 +1,13 @@
+package com.cj;
+
+/**
+ * @author 曹军
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+
+
+    }
+}
