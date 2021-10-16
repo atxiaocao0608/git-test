7ed08c0f3cdb75e98dc4f6e66622943ec1921901
cj <cj@qq.com>
2021-10-16 14:52:45 +0800

    push commit

--- src/main/java/com/cj/GitTest.java
+++ src/main/java/com/cj/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hello Git04");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
 
 
     }
