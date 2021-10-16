df3c54f7545a3d00a3941ea3f34e2d93cab176a2
cj <cj@qq.com>
2021-10-16 10:50:13 +0800

    third commit

--- src/main/java/com/cj/GitTest.java
+++ src/main/java/com/cj/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello Git");
+        System.out.println("hello Git03");
 
 
     }
