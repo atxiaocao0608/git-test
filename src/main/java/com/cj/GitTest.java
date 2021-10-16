24752cf3310426dcde7f79fc6a929903299bcd53
cj <cj@qq.com>
2021-10-16 11:19:34 +0800

    hot-fix commit01

--- src/main/java/com/cj/GitTest.java
+++ src/main/java/com/cj/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello Git03");
         System.out.println("hello Git04");
+        System.out.println("master test");
 
 
     }
